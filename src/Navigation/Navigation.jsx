c95f085be836bac554aa0698492db00efb336746
Cenk Merk <cenkmerk@gmail.com>
2023-08-10 19:44:25 +0300

    Created stack navigation

---
+++ src/Navigation/Navigation.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+const Stack = createNativeStackNavigator();
+
+import OnBoardingScreen from "../Screen/OnBoardingScreen/OnBoardingScreen";
+import RegisterScreen from "../Screen/RegisterScreen/RegisterScreen";
+import LoginScreen from "../Screen/LoginScreen/LoginScreen";
+import ApplicantsScreen from "../Screen/ApplicantsScreen/ApplicantsScreen";
+import AddandEditScreen from "../Screen/AddandEditScreen/AddandEditScreen";
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="OnBoarding" component={OnBoardingScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Applicants" component={ApplicantsScreen} />
+        <Stack.Screen name="AddandEdit" component={AddandEditScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
