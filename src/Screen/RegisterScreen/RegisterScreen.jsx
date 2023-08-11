0ef603d09c741d08f9a1db092677919cf37e352f
Cenk Merk <cenkmerk@gmail.com>
2023-08-11 19:09:37 +0300

    registration screen completed

--- src/Screen/RegisterScreen/RegisterScreen.jsx
+++ src/Screen/RegisterScreen/RegisterScreen.jsx
@@ -1,11 +1,95 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StatusBar,
+  Text,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import React from "react";
 import styles from "./Style";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import ButtonComp from "../../Components/ButtonComp/ButtonComp";
+import InputComt from "../../Components/InputComp/InputComp";
+import { useNavigation } from "@react-navigation/native";
+
+const SignupSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "Too Short!")
+    .max(20, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .min(8, "Must Contain 8 Characters")
+    .required("Required")
+    .matches(/^(?=.*[a-z])/, " Must Contain One Lowercase Character")
+    .matches(/^(?=.*[A-Z])/, "  Must Contain One Uppercase Character")
+    .matches(/^(?=.*[0-9])/, "  Must Contain One Number Character"),
+});
 
 export default function RegisterScreen() {
+  const navigation = useNavigation();
+  const handleSubmit = (values) => {
+    console.log(values);
+    navigation.navigate("OnBoarding");
+  };
+  const formFields = [
+    { name: "name", placeholder: "Full Name" },
+    { name: "email", placeholder: "Email Address" },
+    { name: "password", placeholder: "Password" },
+  ];
   return (
-    <View style={styles.container}>
-      <Text>RegisterScreen</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : null}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
+    >
+      <StatusBar barStyle="dark-content" />
+      <Text style={styles.textHello}>Hello There 🖐</Text>
+      <Text style={styles.title}>Create an account</Text>
+      <ScrollView
+        contentContainerStyle={styles.formContainer}
+        showsVerticalScrollIndicator={false}
+      >
+        <Formik
+          initialValues={{ name: "", email: "", password: "" }}
+          validationSchema={SignupSchema}
+          onSubmit={(values) => handleSubmit(values)}
+        >
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            errors,
+            touched,
+          }) => (
+            <View>
+              {formFields.map((field) => (
+                <React.Fragment key={field.name}>
+                  <InputComt
+                    handleChange={handleChange(field.name)}
+                    handleBlur={handleBlur(field.name)}
+                    value={values[field.name]}
+                    placeholder={field.placeholder}
+                    error={errors[field.name]}
+                    touched={touched[field.name]}
+                    fieldName={field.name}
+                  />
+                </React.Fragment>
+              ))}
+              <ButtonComp
+                btnClick={handleSubmit}
+                btnColor="#FF6464"
+                btnText="Sign Up"
+                btnWidth={327}
+                btnMarginTop={50}
+              />
+            </View>
+          )}
+        </Formik>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
--- src/Screen/RegisterScreen/Style.js
+++ src/Screen/RegisterScreen/Style.js
@@ -1,8 +1,28 @@
 import { StyleSheet } from "react-native";
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#F6F6F6",
+    paddingHorizontal: 20,
+    marginTop: 50,
+  },
+  textHello: {
+    fontWeight: "400",
+    fontSize: 14,
+    color: "#282828",
+    marginTop: 20,
+  },
+  title: {
+    fontWeight: "700",
+    fontSize: 25,
+    marginTop: 10,
+  },
+  formContainer: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 20,
+    marginTop: 20,
   },
 });
 
