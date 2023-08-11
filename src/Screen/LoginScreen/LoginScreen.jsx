8d39a81df8cb9da5a2759e1fd39c3c886686577c
Cenk Merk <cenkmerk@gmail.com>
2023-08-11 19:44:14 +0300

    Create LoginScreen

--- src/Screen/LoginScreen/LoginScreen.jsx
+++ src/Screen/LoginScreen/LoginScreen.jsx
@@ -1,12 +1,93 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StatusBar,
+  Text,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import React from "react";
+import styles from "./LoginScreenStyle";
+import LoginLogoSVG from "../../constants/LoginLogoSVG";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import ButtonComp from "../../Components/ButtonComp/ButtonComp";
+import InputComt from "../../Components/InputComp/InputComp";
+import { useNavigation } from "@react-navigation/native";
+
+const SignupSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .min(8, "Must Contain 8 Characters")
+    .required("Required")
+    .matches(/^(?=.*[a-z])/, " Must Contain One Lowercase Character")
+    .matches(/^(?=.*[A-Z])/, "  Must Contain One Uppercase Character")
+    .matches(/^(?=.*[0-9])/, "  Must Contain One Number Character"),
+});
 
 export default function LoginScreen() {
+  const navigation = useNavigation();
+  const handleSubmit = (values) => {
+    console.log(values);
+    navigation.navigate("Applicants");
+  };
+  const formFields = [
+    { name: "email", placeholder: "Email Address" },
+    { name: "password", placeholder: "Password" },
+  ];
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
-}
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : null}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
+    >
+      <StatusBar barStyle="dark-content" />
 
-const styles = StyleSheet.create({})
+      <ScrollView
+        contentContainerStyle={styles.formContainer}
+        showsVerticalScrollIndicator={false}
+      >
+        <LoginLogoSVG />
+        <Text style={styles.textHello}>Hello There 🖐</Text>
+        <Text style={styles.title}>Login first to continue</Text>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={SignupSchema}
+          onSubmit={(values) => handleSubmit(values)}
+        >
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            errors,
+            touched,
+          }) => (
+            <View>
+              {formFields.map((field) => (
+                <React.Fragment key={field.name}>
+                  <InputComt
+                    handleChange={handleChange(field.name)}
+                    handleBlur={handleBlur(field.name)}
+                    value={values[field.name]}
+                    placeholder={field.placeholder}
+                    error={errors[field.name]}
+                    touched={touched[field.name]}
+                    fieldName={field.name}
+                  />
+                </React.Fragment>
+              ))}
+              <ButtonComp
+                btnClick={handleSubmit}
+                btnColor="#FF6464"
+                btnText="Sign Up"
+                btnWidth={327}
+                btnMarginTop={50}
+              />
+            </View>
+          )}
+        </Formik>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+}
--- src/Screen/LoginScreen/LoginScreenStyle.js
+++ src/Screen/LoginScreen/LoginScreenStyle.js
@@ -5,25 +5,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#F6F6F6",
     paddingHorizontal: 20,
-    paddingTop: 50,
   },
   textHello: {
     fontWeight: "400",
     fontSize: 14,
     color: "#282828",
-    marginTop: 20,
   },
   title: {
     fontWeight: "700",
     fontSize: 25,
-    marginTop: 10,
   },
   formContainer: {
     display: "flex",
     flexDirection: "column",
     gap: 20,
-    marginTop: 20,
   },
 });
-
 export default styles;
