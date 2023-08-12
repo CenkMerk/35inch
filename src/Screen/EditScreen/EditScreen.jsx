b9d740a832c8ce154e8c0eb7da5d78937555cab9
Cenk Merk <cenkmerk@gmail.com>
2023-08-12 23:03:31 +0300

    Created user edit

--- src/Screen/EditScreen/EditScreen.jsx
+++ src/Screen/EditScreen/EditScreen.jsx
@@ -1,10 +1,4 @@
-import {
-  StatusBar,
-  View,
-  KeyboardAvoidingView,
-  Platform,
-  ScrollView,
-} from "react-native";
+import { View, FlatList } from "react-native";
 import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import styles from "./EditScreenStyle";
@@ -16,6 +10,9 @@ import ButtonComp from "../../Components/ButtonComp/ButtonComp";
 import InputComt from "../../Components/InputComp/InputComp";
 //SVG
 import UserAvatarIcon from "../../constants/UserAvatarIcon";
+//Redux
+import { useDispatch } from "react-redux";
+import { removeUser, editUser } from "../../Redux/slice/usersSlice";
 
 const SignupSchema = Yup.object().shape({
   name: Yup.string()
@@ -23,17 +20,13 @@ const SignupSchema = Yup.object().shape({
     .max(20, "Too Long!")
     .required("Required"),
   email: Yup.string().email("Invalid email").required("Required"),
-  password: Yup.string()
-    .min(8, "Must Contain 8 Characters")
-    .required("Required")
-    .matches(/^(?=.*[a-z])/, " Must Contain One Lowercase Character")
-    .matches(/^(?=.*[A-Z])/, "  Must Contain One Uppercase Character")
-    .matches(/^(?=.*[0-9])/, "  Must Contain One Number Character"),
+  phoneNumber: Yup.string().required("Required"),
 });
 
 export default function EditScreen({ route }) {
-  const { email, fullName, phoneNumber, status } = route.params.item;
+  const { email, fullName, phoneNumber, status, id } = route.params.item;
   const navigation = useNavigation();
+  const dispatch = useDispatch();
 
   const [open, setOpen] = useState(false);
   const [statusValue, setStatusValue] = useState(status);
@@ -41,106 +34,102 @@ export default function EditScreen({ route }) {
     { label: "Active", value: "Active" },
     { label: "Passive", value: "Passive" },
   ]);
-  const [formValues, setFormValues] = useState({
-    name: fullName,
-    email: email,
-    phoneNumber: phoneNumber,
-  });
-
- 
 
   const formFields = [
     { name: "name", placeholder: "Full Name" },
     { name: "email", placeholder: "Email Address" },
     { name: "phoneNumber", placeholder: "Phone Number" },
   ];
+
   const handleSubmit = (values) => {
-    // console.log({ values, statusValue });
+    dispatch(editUser({ values, statusValue, id }));
+    navigation.navigate("Users");
+  };
+
+  const HandleDelete = () => {
+    dispatch(removeUser(id));
     navigation.navigate("Users");
   };
 
   return (
-    <KeyboardAvoidingView
-      style={styles.container}
-      behavior={Platform.OS === "ios" ? "padding" : null}
-      keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
-    >
-      <StatusBar barStyle="dark-content" />
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={styles.formContainer}
-      >
-        <View style={styles.AvatarBigContainer}>
-          <View style={styles.AvatarContainer}>
-            <UserAvatarIcon />
-          </View>
+    <View style={styles.container}>
+      <View style={styles.AvatarBigContainer}>
+        <View style={styles.AvatarContainer}>
+          <UserAvatarIcon />
         </View>
-        <Formik
-          initialValues={formValues}
-          validationSchema={SignupSchema}
-          onSubmit={(values) => handleSubmit(values)}
-        >
-          {({
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            values,
-            errors,
-            touched,
-          }) => (
-            <View>
-              {formFields.map((field) => (
-                <React.Fragment key={field.name}>
+      </View>
+      <Formik
+        initialValues={{
+          name: fullName,
+          email: email,
+          phoneNumber: phoneNumber,
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={(values) => handleSubmit(values)}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          touched,
+        }) => (
+          <View>
+            <FlatList
+              data={formFields}
+              renderItem={(item) => {
+                return (
                   <InputComt
-                    handleChange={handleChange(field.name)}
-                    handleBlur={handleBlur(field.name)}
-                    value={values[field.name]}
-                    placeholder={field.placeholder}
-                    error={errors[field.name]}
-                    touched={touched[field.name]}
-                    fieldName={field.name}
+                    handleChange={handleChange(item.item.name)}
+                    handleBlur={handleBlur(item.item.name)}
+                    value={values[item.item.name]}
+                    placeholder={item.item.placeholder}
+                    error={errors[item.item.name]}
+                    touched={touched[item.item.name]}
+                    fieldName={item.item.name}
                   />
-                </React.Fragment>
-              ))}
-              <DropDownPicker
-                open={open}
-                value={statusValue}
-                items={statusItems}
-                setOpen={setOpen}
-                setValue={setStatusValue}
-                setItems={setStatusItems}
-                placeholder="Status"
-                placeholderStyle={{
-                  color: "#28282866",
-                  fontWeight: "600",
-                }}
-                style={{
-                  borderWidth: "none",
-                  marginTop: 20,
-                }}
-                dropDownContainerStyle={{
-                  borderWidth: 0,
-                  marginTop: 10,
-                }}
-              />
-              <ButtonComp
-                btnClick={handleSubmit}
-                btnColor="#42CD00"
-                btnText="Edit"
-                btnWidth={327}
-                btnMarginTop={50}
-              />
-               <ButtonComp
-                btnClick={handleSubmit}
-                btnColor="#FF6464"
-                btnText="Delete"
-                btnWidth={327}
-                btnMarginTop={10}
-              />
-            </View>
-          )}
-        </Formik>
-      </ScrollView>
-    </KeyboardAvoidingView>
+                );
+              }}
+            />
+            <DropDownPicker
+              open={open}
+              value={statusValue}
+              items={statusItems}
+              setOpen={setOpen}
+              setValue={setStatusValue}
+              setItems={setStatusItems}
+              placeholder="Status"
+              placeholderStyle={{
+                color: "#28282866",
+                fontWeight: "600",
+              }}
+              style={{
+                borderWidth: "none",
+                marginTop: 20,
+              }}
+              dropDownContainerStyle={{
+                borderWidth: 0,
+                marginTop: 10,
+              }}
+            />
+            <ButtonComp
+              btnClick={handleSubmit}
+              btnColor="#42CD00"
+              btnText="Edit"
+              btnWidth={327}
+              btnMarginTop={50}
+            />
+            <ButtonComp
+              btnClick={HandleDelete}
+              btnColor="#FF6464"
+              btnText="Delete"
+              btnWidth={327}
+              btnMarginTop={10}
+            />
+          </View>
+        )}
+      </Formik>
+    </View>
   );
 }
