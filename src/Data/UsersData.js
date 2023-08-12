a5b69aff8fa1ce8e62d1650411759d472ed37bc4
Cenk Merk <cenkmerk@gmail.com>
2023-08-12 23:02:19 +0300

    Updated data

--- src/Data/UsersData.js
+++ src/Data/UsersData.js
@@ -1,23 +1,27 @@
 export default usersData = [
   {
+    id:Math.floor(Math.random() * 9999),
     fullName: "Ralph Foster",
     phoneNumber: "0533 333 33 33",
     email: "test@35inch.com",
     status: "Active",
   },
   {
+    id:Math.floor(Math.random() * 9999),
     fullName: "Willie Butler",
     phoneNumber: "0533 333 33 33",
     email: "test@35inch.com",
     status: "Active",
   },
   {
+    id:Math.floor(Math.random() * 9999),
     fullName: "Roger Gutierrez",
     phoneNumber: "0533 333 33 33",
     email: "test@35inch.com",
     status: "Active",
   },
   {
+    id:Math.floor(Math.random() * 9999),
     fullName: "Jack Howard",
     phoneNumber: "0533 333 33 33",
     email: "test@35inch.com",
