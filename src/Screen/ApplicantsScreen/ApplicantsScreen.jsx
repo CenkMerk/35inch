722a65dcf373e636bc2ca232ace829a3e6773284
Cenk Merk <cenkmerk@gmail.com>
2023-08-12 23:05:14 +0300

    Added redux

--- src/Screen/ApplicantsScreen/ApplicantsScreen.jsx
+++ src/Screen/ApplicantsScreen/ApplicantsScreen.jsx
@@ -1,17 +1,18 @@
 import { View, FlatList } from "react-native";
 import React from "react";
 import styles from "./ApplicantScreenStyle";
-//data
-import UsersData from "../../Data/UsersData";
 //component
 import UserItem from "../../Components/UserItem/UserItem";
-
+//redux
+import { useSelector } from "react-redux";
 
 export default function ApplicantsScreen() {
+  const { usersData } = useSelector((store) => store.users);
   return (
     <View style={styles.container}>
       <FlatList
-        data={UsersData}
+        showsVerticalScrollIndicator={false}
+        data={usersData}
         renderItem={({ item }) => {
           return <UserItem item={item} />;
         }}
